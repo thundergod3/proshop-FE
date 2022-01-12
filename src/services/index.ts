879357a433411d7409a40774e93f79ca2eeb2410
Cong Phan <congthundergodphan@gmail.com>
2022-01-13 00:35:04 +0700

    Update config

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,35 +1,44 @@
 import axios, { AxiosStatic } from "axios";
 
 class HTTPMethod {
-	axios: AxiosStatic;
+  axios: AxiosStatic;
 
-	constructor() {
-		this.axios = axios;
-		this.axios.defaults.baseURL = "https://proshop-app-fullstack.herokuapp.com";
-		this.axios.defaults.headers = { "Access-Control-Allow-Origin": "*", "Content-type": "application/json" };
-	}
+  constructor() {
+    this.axios = axios;
+    this.axios.defaults.baseURL = "http://localhost:8080";
+    this.axios.defaults.headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Content-type": "application/json",
+    };
+  }
 
-	get = (url: string, remainProps?: any): Promise<any> => axios.get(url, remainProps);
+  get = (url: string, remainProps?: any): Promise<any> =>
+    axios.get(url, remainProps);
 
-	post = (url: string, remainProps?: any): Promise<any> => axios.post(url, remainProps);
+  post = (url: string, remainProps?: any): Promise<any> =>
+    axios.post(url, remainProps);
 
-	put = (url: string, remainProps?: any): Promise<any> => axios.put(url, remainProps);
+  put = (url: string, remainProps?: any): Promise<any> =>
+    axios.put(url, remainProps);
 
-	delete = (url: string, remainProps?: any): Promise<any> => axios.delete(url, remainProps);
+  delete = (url: string, remainProps?: any): Promise<any> =>
+    axios.delete(url, remainProps);
 
-	attachTokenToHeader = ({ token }: any): string => (axios.defaults.headers["Authorization"] = `Bearer ${token}`);
+  attachTokenToHeader = ({ token }: any): string =>
+    (axios.defaults.headers["Authorization"] = `Bearer ${token}`);
 
-	removeTokenToHeader = (): string => (axios.defaults.headers["Authorization"] = "");
+  removeTokenToHeader = (): string =>
+    (axios.defaults.headers["Authorization"] = "");
 
-	uploadImage = (image: any) => {
-		const config = {
-			headers: {
-				"Content-Type": "multipart/form-data",
-			},
-		};
+  uploadImage = (image: any) => {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
 
-		return axios.post("/api/admin/upload-image", image, config);
-	};
+    return axios.post("/api/admin/upload-image", image, config);
+  };
 }
 
 export default new HTTPMethod();
